9fb65c05b98c8a7cdda9c622ea069e47f9de8bed
Binore Mohapatra <binore.mohapatra@gmail.com>
2026-05-01 12:59:27 +0530

    Add Java backend source files

--- backend/src/main/java/com/maeve/Application.java
+++ backend/src/main/java/com/maeve/Application.java
@@ -1,5 +1,7 @@
 package com.maeve;
 
+// Spring Boot application for Maeve AI
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
